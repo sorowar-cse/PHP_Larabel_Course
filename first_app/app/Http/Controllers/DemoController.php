3b1894a597316e021c412441958c9a3fb171b8ad
Sorowar Mahabub <sorowarmahabub.bsc.cse.iiuc@gmail.com>
2022-12-01 19:37:55 +0600

    Class-8_Conntecting Route+Controller+View

--- first_app/app/Http/Controllers/DemoController.php
+++ first_app/app/Http/Controllers/DemoController.php
@@ -7,14 +7,17 @@ use Illuminate\Http\Request;
 class DemoController extends Controller
 {
     function Home1(){
-        return "I am Home1 from DemoController";
+        // return "I am Home1 from DemoController";
+        return view('Home');
     }
 
     function About1(){
-        return "I am About1 from DemoController";
+        // return "I am About1 from DemoController";
+        return view('about');
     }
 
     function Contact1(){
-        return "I am Contact1 from DemoController";
+        // return "I am Contact1 from DemoController";
+        return view('contact');
     }
 }
