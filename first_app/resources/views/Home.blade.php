50d55e9f4429aab99b83e371a540b5a75e9ce77c
Sorowar Mahabub <sorowarmahabub.bsc.cse.iiuc@gmail.com>
2022-12-01 19:49:14 +0600

    Class_11-Routing With Hyperlink

--- first_app/resources/views/Home.blade.php
+++ first_app/resources/views/Home.blade.php
@@ -1,1 +1,10 @@
 <h1>This is Home Page</h1>
+
+<!-- // Path: resources\views\Home.blade.php -->
+<a href="{{url('/')}}">Home</a><br>
+
+<!-- // Path: resources\views\About.blade.php -->
+<a href="{{url('about')}}">About</a><br>
+
+<!-- // Path: resources\views\Contact.blade.php -->
+<a href="{{url('contact')}}">Contact</a><br>
